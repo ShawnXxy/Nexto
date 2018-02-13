a5a815d6f8dfd9aa0f8b0d9284605ec6e0c10048
ShawnXxy <shawnxxy@hotmail.com>
2018-02-13 14:35:44 -0600

    bug fixed

--- app/src/main/java/site/shawnxxy/nexto/AddTaskActivity.java
+++ app/src/main/java/site/shawnxxy/nexto/AddTaskActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import site.shawnxxy.nexto.data.TaskContract;
@@ -16,23 +17,19 @@ public class AddTaskActivity extends AppCompatActivity {
 	// keep track of a tasks selected priority
 	private int mPriority;
 
-	RadioButton radio1 = findViewById(R.id.Radio1);
-	RadioButton radio2 = findViewById(R.id.Radio2);
-	RadioButton radio3 = findViewById(R.id.Radio3);
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_task);
 
-		radio1.setChecked(true);
+		((RadioButton)findViewById(R.id.radio1)).setChecked(true);
 		mPriority = 1;
 	}
 
 	// Add task to database when Add button is clicked
 	public void onClickAddTask(View view) {
 		//  check if input is empty
-		String input = ((EditText) findViewById(R.id.TaskDescriptionEditText)).getText().toString();
+		String input = ((TextView) findViewById(R.id.TaskDescriptionEditText)).getText().toString();
 		if (input.length() == 0) {
 			return;
 		}
@@ -49,12 +46,12 @@ public class AddTaskActivity extends AppCompatActivity {
 		finish();
 	}
 
-	public void onPrioritySeleted(View view) {
-		if (radio1.isChecked()) {
+	public void onPrioritySelected(View view) {
+		if (((RadioButton)findViewById(R.id.radio1)).isChecked()) {
 			mPriority = 1;
-		} else if (radio2.isChecked()) {
+		} else if (((RadioButton)findViewById(R.id.radio2)).isChecked()) {
 			mPriority = 2;
-		} else if (radio3.isChecked()) {
+		} else if (((RadioButton)findViewById(R.id.radio3)).isChecked()) {
 			mPriority = 3;
 		}
 	}
