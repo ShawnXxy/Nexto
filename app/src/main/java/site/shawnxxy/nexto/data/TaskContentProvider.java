e9b5292bb804e42f2e91d6115526fced1c3f6602
ShawnXxy <shawnxxy@hotmail.com>
2018-02-13 11:33:00 -0600

    add a UriMatcher

--- app/src/main/java/site/shawnxxy/nexto/data/TaskContentProvider.java
+++ app/src/main/java/site/shawnxxy/nexto/data/TaskContentProvider.java
@@ -3,6 +3,7 @@ package site.shawnxxy.nexto.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -13,6 +14,17 @@ import android.support.annotation.Nullable;
  */
 
 public class TaskContentProvider extends ContentProvider {
+
+	public static final int TASKS = 100;
+	public static final int TASK_WITH_ID = 101;
+	private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+	public static UriMatcher buildUriMatcher() {
+		UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+		uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
+		uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
+		return uriMatcher;
+	}
 
 	private TaskDbHelper mTaskDbHelper;
 
