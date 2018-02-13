79b9f4b67c369d44a2ce1c5d01be001b5020dcce
ShawnXxy <shawnxxy@hotmail.com>
2018-02-13 10:59:19 -0600

    Add URIs to Contract

--- app/src/main/java/site/shawnxxy/nexto/data/TaskContract.java
+++ app/src/main/java/site/shawnxxy/nexto/data/TaskContract.java
@@ -1,5 +1,6 @@
 package site.shawnxxy.nexto.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,7 +9,15 @@ import android.provider.BaseColumns;
 
 public class TaskContract {
 
+	// Add content provider
+	public static final String AUTHORITY = "site.shawnxxy.nexto";
+	public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+	public static final String PATH_TASKS = "tasks";
+
 	public static final class TaskEntry implements BaseColumns {
+
+		public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
+
 		public static final String TABLE_NAME = "tasks";
 		public static final String COLUMN_DESCRIPTION = "description";
 		public static final String COLUMN_PRIORITY = "priority";
